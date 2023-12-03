9f37a0ab58916ce1bbd0994de9c2ec41885aab28
naomi <naomina@me.com>
2023-12-03 20:45:39 +0900

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,52 +1,52 @@
-import React, { useState } from "react";
-import { graphql } from "gatsby";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
+import React, { useState } from "react"
+import { graphql } from "gatsby"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
 
-import Layout from "../components/layout";
+import Layout from "../components/layout"
 import Seo from "../components/seo"
 import BreadCrumbList from "../components/breadcrumb-list"
 
 import styled from "styled-components"
 const Contact = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title;
-  const author = data.site.siteMetadata.author.name;
+  const siteTitle = data.site.siteMetadata.title
+  const author = data.site.siteMetadata.author.name
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [subject, setSubject] = useState("");
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [subject, setSubject] = useState("")
+  const [message, setMessage] = useState("")
 
   const handleChange = event => {
     switch (event.target.name) {
       case "name":
-        setName(event.target.value);
-        break;
+        setName(event.target.value)
+        break
       case "email":
-        setEmail(event.target.value);
-        break;
+        setEmail(event.target.value)
+        break
       case "subject":
-        setSubject(event.target.value);
-        break;
+        setSubject(event.target.value)
+        break
       case "message":
-        setMessage(event.target.value);
-        break;
+        setMessage(event.target.value)
+        break
       default:
-        console.log("key not found");
+        console.log("key not found")
     }
-  };
+  }
 
   const canSubmit = () => {
-    if (name === "") return true;
-    if (email === "") return true;
-    if (subject === "") return true;
-    if (message === "") return true;
+    if (name === "") return true
+    if (email === "") return true
+    if (subject === "") return true
+    if (message === "") return true
 
-    return false;
-  };
+    return false
+  }
 
   return (
-      <Layout location={location} title={siteTitle} author={author}>
+    <Layout location={location} title={siteTitle} author={author}>
       <Article
         className="blog-post"
         itemScope
@@ -64,13 +64,19 @@ const Contact = ({ data, location }) => {
             できる限り対応させていただきますが、内容によっては返信を控えさせて頂きますのでご了承ください。
           </p>
 
-          <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+          <form
+            name="contact"
+            netlify
+            method="POST"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+          >
             <input type="hidden" name="form-name" value="contact" />
             <input type="hidden" name="bot-field" />
-            <div className='contact_area'>
+            <div className="contact_area">
               <TextField
                 id="name"
-                className='contact_name'
+                className="contact_name"
                 name="name"
                 label="お名前"
                 type="text"
@@ -79,10 +85,10 @@ const Contact = ({ data, location }) => {
                 onChange={handleChange}
               />
             </div>
-            <div className='contact_area'>
+            <div className="contact_area">
               <TextField
                 id="email"
-                className='contact_email'
+                className="contact_email"
                 name="email"
                 label="メールアドレス"
                 type="email"
@@ -91,10 +97,10 @@ const Contact = ({ data, location }) => {
                 onChange={handleChange}
               />
             </div>
-            <div className='contact_area'>
+            <div className="contact_area">
               <TextField
                 id="subject"
-                className='contact_subject'
+                className="contact_subject"
                 name="subject"
                 label="件名"
                 type="text"
@@ -103,10 +109,10 @@ const Contact = ({ data, location }) => {
                 onChange={handleChange}
               />
             </div>
-            <div className='contact_area'>
+            <div className="contact_area">
               <TextField
                 id="message"
-                className='contact_msg'
+                className="contact_msg"
                 name="message"
                 label="お問い合わせ内容"
                 multiline
@@ -128,18 +134,15 @@ const Contact = ({ data, location }) => {
             </Button>
           </form>
         </div>
-        </Article>
-      </Layout>
-  );
-};
+      </Article>
+    </Layout>
+  )
+}
 
-export default Contact;
+export default Contact
 
-export const Head = ({ data,location }) => (
-   <Seo
-     title="お問合わせ | ナオのメンタルヘルス"
-     location={location}
-   />
+export const Head = ({ data, location }) => (
+  <Seo title="お問合わせ | ナオのメンタルヘルス" location={location} />
 )
 
 export const pageQuery = graphql`
@@ -153,7 +156,7 @@ export const pageQuery = graphql`
       }
     }
   }
-`;
+`
 
 const Article = styled.article`
   max-width: 750px;
